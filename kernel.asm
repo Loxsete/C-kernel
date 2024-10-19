28bde6c9466b819796cfbc6d5b51796a72bfd61f
Loxsete <gduck3029@gmail.com>
2024-10-19 10:20:24 +0300

    Update kernel.asm

--- kernel.asm
+++ kernel.asm
@@ -1,20 +1,13 @@
 ;;kernel.asm
-
-;nasm directive - 32 bit
-bits 32
+bits 32			;директива nasm - 32 bit
 section .text
-        ;multiboot spec
-        align 4
-        dd 0x1BADB002            ;магические числа
-        dd 0x00                  ;флаги
-        dd - (0x1BADB002 + 0x00) ;контрольная сумма. мч+ф+кс должно равняться нулю
 
 global start
-extern kmain	        ;kmain определена во внешнем файле
+extern kmain	        ;kmain определена в C-файле
 
 start:
   cli 			;блокировка прерываний
-  mov esp, stack_space	;указатель стека
+  mov esp, stack_space	;установка указателя стека
   call kmain
   hlt		 	;остановка процессора
 
