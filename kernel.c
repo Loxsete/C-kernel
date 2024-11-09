74df87c0c7118d34a377347e64cda82157594b96
Loxsete <gduck3029@gmail.com>
2024-11-09 10:11:03 +0200

    Update kernel.c
    Added calculator

--- kernel.c
+++ kernel.c
@@ -33,6 +33,7 @@ struct IDT_entry {
 
 struct IDT_entry IDT[IDT_SIZE];
 
+void calc(void);
 void kprint(const char *str);
 void clear_screen(void);
 void kprint_newline(void);
@@ -124,7 +125,7 @@ void idt_init(void) {
     IDT[0x21].zero = 0;
     IDT[0x21].type_attr = 0x8e;
     IDT[0x21].offset_higherbits = (keyboard_address & 0xffff0000) >> 16;
-    
+
     write_port(0x20, 0x11);
     write_port(0xA0, 0x11);
     write_port(0x21, 0x20);
@@ -135,11 +136,11 @@ void idt_init(void) {
     write_port(0xA1, 0x01);
     write_port(0x21, 0xff);
     write_port(0xA1, 0xff);
-    
+
     idt_address = (unsigned long)IDT;
     idt_ptr[0] = (sizeof(struct IDT_entry) * 256) + ((idt_address & 0xffff) << 16);
     idt_ptr[1] = idt_address >> 16;
-    
+
     load_idt(idt_ptr);
 }
 
@@ -165,10 +166,11 @@ void keyboard_handler_main(void) {
             if (buffer_index > 0) {
                 if (my_strcmp(input_buffer, "help") == 0) {
                     kprint("Available commands:\n");
-                    kprint("1. help - Show this help message\n");
-                    kprint("2. clear - Clear the screen\n");
-                    kprint("3. info - Show system information\n");
-                    kprint("4. shutdown - Shutdown the system\n");
+                    kprint("1) help - Show this help message\n");
+                    kprint("2) clear - Clear the screen\n");
+                    kprint("3) info - Show system information\n");
+                    kprint("4) shutdown - Shutdown the system\n");
+                    kprint("5) calc - Open calculator\n");
                 } 
                 else if (my_strcmp(input_buffer, "clear") == 0) {
                     clear_screen();
@@ -182,6 +184,8 @@ void keyboard_handler_main(void) {
                     kprint("Shutting down...\n");
                     asm volatile ("movl $0, %eax\n\t"
                                   "int $0x19");
+                } else if (my_strcmp(input_buffer, "calc") == 0) {
+                    calc();
                 } else {
                     kprint("Unknown command.\n");
                 }
@@ -204,15 +208,89 @@ void keyboard_handler_main(void) {
     }
 }
 
+void calc(void) {
+    int num1 = 0, num2 = 0, result = 0;
+    char operator[10] = {0};
+    char input[256];
+    int input_index = 0;
+
+    kprint("Enter calculation (e.g., 4plus4):\n");
+
+    while (1) {
+        unsigned char status = read_port(KEYBOARD_STATUS_PORT);
+        if (status & 0x01) {
+            char keycode = read_port(KEYBOARD_DATA_PORT);
+            if (keycode == ENTER_KEY_CODE) {
+                input[input_index] = '\0';
+                break;
+            } else if (keycode == BACKSPACE_KEY_CODE && input_index > 0) {
+                input_index--;
+                current_loc -= 2;
+                vidptr[current_loc] = ' ';
+            } else {
+                char ascii = keyboard_map[(unsigned char)keycode];
+                if (ascii != 0) {
+                    input[input_index++] = ascii;
+                    vidptr[current_loc++] = ascii;
+                    vidptr[current_loc++] = 0x07;
+                }
+            }
+        }
+    }
+
+    char num1_str[10] = {0}, num2_str[10] = {0};
+    int i = 0, j = 0;
+
+    while (input[i] != '\0' && (input[i] >= '0' && input[i] <= '9' || input[i] == '-')) {
+        num1_str[j++] = input[i++];
+    }
+    num1_str[j] = '\0';
+    num1 = simple_atoi(num1_str);
+
+    j = 0;
+    while (input[i] != '\0' && !(input[i] >= '0' && input[i] <= '9')) {
+        operator[j++] = input[i++];
+    }
+    operator[j] = '\0';
+
+    j = 0;
+    while (input[i] != '\0' && (input[i] >= '0' && input[i] <= '9' || input[i] == '-')) {
+        num2_str[j++] = input[i++];
+    }
+    num2_str[j] = '\0';
+    num2 = simple_atoi(num2_str);
+
+    if (my_strcmp(operator, "plus") == 0) {
+        result = num1 + num2;
+    } else if (my_strcmp(operator, "minus") == 0) {
+        result = num1 - num2;
+    } else if (my_strcmp(operator, "times") == 0) {
+        result = num1 * num2;
+    } else if (my_strcmp(operator, "divide") == 0) {
+        if (num2 == 0) {
+            kprint("Error: Division by zero\n");
+            return;
+        }
+        result = num1 / num2;
+    } else {
+        kprint("Error: Invalid operation\n");
+        return;
+    }
+
+    char result_str[20];
+    simple_itoa(result, result_str);
+    kprint("Result: ");
+    kprint(result_str);
+}
+
 void kmain(void) {
     clear_screen();
     kprint("Welcome to Loxsete-OS!\n");
-    kprint("Type 'help' to see available commands.\n\n");
-
+    kprint("Type 'help' to see the list of commands.\n");
+    kprint("\n");
     idt_init();
     kb_init();
-    
+
     while (1) {
-        // Основной цикл работы ОС
     }
 }
