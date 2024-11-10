a0ce8db365187b82ce83099acf30232b4ce1b7f0
Loxsete <gduck3029@gmail.com>
2024-11-10 19:58:54 +0200

    Update kernel.asm

--- kernel.asm
+++ kernel.asm
@@ -1,13 +1,9 @@
-; Copyright (C) 2014  Arjun Sreedharan
-; License: GPL version 2 or higher http://www.gnu.org/licenses/gpl.html
-
 bits 32
 section .text
-        ;multiboot spec
-        align 4
-        dd 0x1BADB002              ;magic
-        dd 0x00                    ;flags
-        dd - (0x1BADB002 + 0x00)   ;checksum. m+f+c should be zero
+align 4
+dd 0x1BADB002
+dd 0x00
+dd - (0x1BADB002 + 0x00)
 
 global start
 global keyboard_handler
@@ -15,37 +11,36 @@ global read_port
 global write_port
 global load_idt
 
-extern kmain 		;this is defined in the c file
+extern kmain
 extern keyboard_handler_main
 
 read_port:
 	mov edx, [esp + 4]
-			;al is the lower 8 bits of eax
-	in al, dx	;dx is the lower 16 bits of edx
+	in al, dx
 	ret
 
 write_port:
-	mov   edx, [esp + 4]    
-	mov   al, [esp + 4 + 4]  
-	out   dx, al  
+	mov edx, [esp + 4]
+	mov al, [esp + 4 + 4]
+	out dx, al
 	ret
 
 load_idt:
 	mov edx, [esp + 4]
 	lidt [edx]
-	sti 				;turn on interrupts
+	sti
 	ret
 
-keyboard_handler:                 
-	call    keyboard_handler_main
+keyboard_handler:
+	call keyboard_handler_main
 	iretd
 
 start:
-	cli 				;block interrupts
+	cli
 	mov esp, stack_space
 	call kmain
-	hlt 				;halt the CPU
+	hlt
 
 section .bss
-resb 8192; 8KB for stack
+resb 8192
 stack_space:
