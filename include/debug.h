39b9e742877eda57531213f1cde5d433e70de3f5
Void User <loxsete@localhost.localdomain>
2025-07-12 22:42:10 +0300

    reforms

--- include/debug.h
+++ include/debug.h
@@ -1,7 +1,7 @@
 #ifndef DEBUG_H
 #define DEBUG_H
 
-#include "graphics.h"
+#include "figures.h"
 
 void debug_print(const char *msg, Color color);
 void debug_print_hex(const char *prefix, unsigned char value);
